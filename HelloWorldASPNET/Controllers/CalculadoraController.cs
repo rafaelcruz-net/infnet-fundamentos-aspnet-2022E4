b0f67eb7af10850687c201071930bd1929b18a3e
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2022-10-17 09:07:46 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWorldASPNET/Controllers/CalculadoraController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldASPNET.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public IActionResult Calcular(int number1, int number2)
+        {
+            ViewBag.Resultado = number1 + number2;
+
+            return View();
+        }
+    }
+}
---
+++ HelloWorldASPNET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HelloWorldASPNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
